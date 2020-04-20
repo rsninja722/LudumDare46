654da668486f2af1223b9c48ad5c8f51944dbe5c
William <Wcmeathrel@gmail.com>
2020-04-20 17:25:53 -0400

    updated credits

--- docs/assets/js/titleScreen/titleScreen.js
+++ docs/assets/js/titleScreen/titleScreen.js
@@ -26,7 +26,6 @@ function handleTitleScreen() {
 function handleMainScreen(){
     if(rectpoint({x:415, y:200, w: 300, h: 50}, {x:cursor.x, y:cursor.y}) && mouseDown[0]){
         globalState = globalStates.levelTransition;
-        timer = 0;
 
         // Play the bgm
         soundAssets.backingtrack.playForever();
@@ -35,51 +34,57 @@ function handleMainScreen(){
     if(timer > 20){
         if(rectpoint({x:415, y:550, w: 300, h: 50}, {x:cursor.x, y:cursor.y}) && mouseDown[0]){
             titleScreenState = "credits"
-            timer = 0;
+            
         }
-    } else {
+    }else{
         timer++;
     }
 }
 
 
 function handleCredits(){
-    //console.log(`X ${cursor.x}, Y ${cursor.y}`);
+    
+    
+    
     if(timer > 20){
-        if(rectpoint({x:395, y:550, w: 140, h: 50}, {x:cursor.x, y:cursor.y}) && mouseDown[0]){
+        if(rectpoint({x:705, y:550, w: 140, h: 50}, {x:cursor.x, y:cursor.y}) && mousePress[0]){
             titleScreenState = "main";
             timer = 0;
         }
 
         // Rsninja
-        if(rectpoint({x:400, y:145, w: 340, h: 30}, {x:cursor.x, y:cursor.y}) && mouseDown[0]){
+        if(rectpoint({x:400, y:145, w: 340, h: 30}, {x:cursor.x, y:cursor.y}) && mousePress[0]){
             window.open('https://rsninja.dev/', '_blank');
             timer = 0;
         }
 
         // Silas
-        if(rectpoint({x:420, y:227, w: 350, h: 31}, {x:cursor.x, y:cursor.y}) && mouseDown[0]){
+        if(rectpoint({x:420, y:227, w: 350, h: 31}, {x:cursor.x, y:cursor.y}) && mousePress[0]){
             window.open('https://exvacuum.dev', '_blank');
             timer = 0;
         }
 
         //Evan
-        if(rectpoint({x:430, y:307, w: 360, h: 50}, {x:cursor.x, y:cursor.y}) && mouseDown[0]){
+        if(rectpoint({x:430, y:307, w: 360, h: 50}, {x:cursor.x, y:cursor.y}) && mousePress[0]){
             window.open('https://retrylife.ca/', '_blank');
             timer = 0;
         }
 
-        // // William
-        if(rectpoint({x:460, y:382, w: 420, h: 50}, {x:cursor.x, y:cursor.y}) && mouseDown[0]){
+        // William
+        if(rectpoint({x:460, y:382, w: 420, h: 50}, {x:cursor.x, y:cursor.y}) && mousePress[0]){
             window.open('https://wm-c.dev', '_blank');
             timer = 0;
         }
-
+        
+        // MarshMellow
+        if(rectpoint({x:460, y:462, w: 420, h: 50}, {x:cursor.x, y:cursor.y}) && mousePress[0]){
+            window.open('https://www.youtube.com/channel/UC5TeGG-Ak6ouX5JdxZB-ESg', '_blank');
+            timer = 0;
+        }
     }else{
         timer++;
     }
-
-    
+  
         
 
 }
@@ -106,14 +111,16 @@ function drawTitleScreen() {
 
         text("Evan Pratten", 250, 290, "green", 5, 1000);
 
-        text("William Meathrel", 250, 370, "green", 5, 1000)
+        text("William Meathrel", 250, 370, "green", 5, 1000);
 
-        //text("Sally Lopez", 250, 320, "green", 5, 1000)
+        text("MarshMellow", 250, 450, "green", 5, 1000);
 
-        rect(395, 550, 140, 50, "green");
-        text("Back", 345, 535, "white", 5, 150);
+        text("Sally Lopez", 250, 530, "green", 5, 1000)
 
-        text("*Names are links to their pages", 705, 495, "green", 2, 100);
+        rect(705, 550, 140, 50, "green");
+        text("Back", 650, 535, "white", 5, 150);
+
+        text("*Names are links to their pages", 20, 495, "green", 2, 100);
 
     }
 
